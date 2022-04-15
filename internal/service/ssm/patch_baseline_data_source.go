3cb1a32ef019ec9a57207ec53a15e3db88f56f7e
Sofia Nieves <sofia+git@qwfp.net>
2022-04-14 20:44:52 -0400

    Use GetPatchBaseline to obtain patch baseline information
    It looks like the only bit of data that GetPatchBaseline is missing that
    DescribePatchBaselines provides is default_baseline.

--- internal/service/ssm/patch_baseline_data_source.go
+++ internal/service/ssm/patch_baseline_data_source.go
@@ -3,12 +3,15 @@ package ssm
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 )
 
 func DataSourcePatchBaseline() *schema.Resource {
@@ -106,13 +109,45 @@ func dataPatchBaselineRead(d *schema.ResourceData, meta interface{}) error {
 		return fmt.Errorf("Your query returned more than one result. Please try a more specific search criteria")
 	}
 
-	baseline := filteredBaselines[0]
+	baseline, err := conn.GetPatchBaseline(&ssm.GetPatchBaselineInput{
+		BaselineId: filteredBaselines[0].BaselineId,
+	})
+
+	if err != nil {
+		return fmt.Errorf("Error getting SSM PatchBaseline: %w", err)
+	}
 
 	d.SetId(aws.StringValue(baseline.BaselineId))
-	d.Set("name", baseline.BaselineName)
-	d.Set("description", baseline.BaselineDescription)
-	d.Set("default_baseline", baseline.DefaultBaseline)
+	d.Set("name", baseline.Name)
+	d.Set("description", baseline.Description)
+	d.Set("default_baseline", filteredBaselines[0].DefaultBaseline)
 	d.Set("operating_system", baseline.OperatingSystem)
+	d.Set("approved_patches_compliance_level", baseline.ApprovedPatchesComplianceLevel)
+	d.Set("approved_patches", flex.FlattenStringList(baseline.ApprovedPatches))
+	d.Set("rejected_patches", flex.FlattenStringList(baseline.RejectedPatches))
+	d.Set("rejected_patches_action", baseline.RejectedPatchesAction)
+	d.Set("approved_patches_enable_non_security", baseline.ApprovedPatchesEnableNonSecurity)
+
+	if err := d.Set("global_filter", flattenPatchFilterGroup(baseline.GlobalFilters)); err != nil {
+		return fmt.Errorf("Error setting global filters error: %#v", err)
+	}
+
+	if err := d.Set("approval_rule", flattenPatchRuleGroup(baseline.ApprovalRules)); err != nil {
+		return fmt.Errorf("Error setting approval rules error: %#v", err)
+	}
+
+	if err := d.Set("source", flattenPatchSource(baseline.Sources)); err != nil {
+		return fmt.Errorf("Error setting patch sources error: %#v", err)
+	}
+
+	arn := arn.ARN{
+		Partition: meta.(*conns.AWSClient).Partition,
+		Region:    meta.(*conns.AWSClient).Region,
+		Service:   "ssm",
+		AccountID: meta.(*conns.AWSClient).AccountID,
+		Resource:  fmt.Sprintf("patchbaseline/%s", strings.TrimPrefix(d.Id(), "/")),
+	}
+	d.Set("arn", arn.String())
 
 	return nil
 }
